4ab41312dd0ee7ff2f6dba8fa54203bd6ac7a3e8
Ashish Rangra <ashishrangra45@gmail.com>
2024-04-27 15:22:23 +0530

    commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import Body from "./Body";
 import { Link } from "react-router-dom";
-// import "./Body.css";
+import "./Body.css";
 import { CiShoppingCart } from "react-icons/ci";
+import { Context } from "../Context/Context";
 const Home = () => {
+  const {cartItem}=useContext(Context);
+  // const [countCartProducts,setCountCartProducts]=useState('');
+  // const totalSelectedProducts=(productsCount)=>{
+  //   setCountCartProducts(productsCount)
+  // }
+  
   return (
     <>
       <div className="navbar">
         <Link to="/cart">
           <CiShoppingCart className="cartIcon" size={50} />
-        </Link>
+        </Link><span className="cartCount">{Object.keys(cartItem).length}</span>
       </div>
       <div>
         <Body />
